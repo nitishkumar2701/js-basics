c7595714de525f5b20947399474854ec0ac464bd
Nitish Kumar S <62009174+nitishkumar2701@users.noreply.github.com>
2021-11-08 11:35:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,53 @@
+// alert("Hello World");
+
+// let a = 10;
+// let b = 5;
+// let c = a+b;
+// alert(c);
+
+// let num = prompt("enter the number");
+// let square_num = num*num;
+// console.log("the square is",square_num);
+
+// let names = "divyan"
+
+// let str = prompt("enter the name:");
+
+// if(str == names){
+//     console.log("match found");
+// }
+// else{
+//     console.log("match not found");
+// }
+
+// for (let i = 1; i <=5; i++) {
+//    console.log(i);
+// }
+
+// let i=1
+// while (i<2) {
+// console.log("hello");
+// break;
+// }
+
+
+// let num1 =10;
+// let num2 =20;
+
+// function add(a,b){
+//     let c = a+b;
+//     console.log(c);
+// }
+
+// add(num1,num2);
+
+// document.querySelector('h1').style.color="red";
+
+
+// function gettingInput(){
+//      let username = document.getElementById("uName").value;
+//      let password = document.getElementById("uPass").value;
+//     document.querySelector("h2").innerText=username;
+//     document.querySelector("h3").innerText=password;
+// }
+
