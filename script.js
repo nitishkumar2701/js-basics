4e6cdddb687030882d4bf0b5a5149b77093e4d3b
Nitish Kumar S <62009174+nitishkumar2701@users.noreply.github.com>
2021-11-08 11:38:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 // alert("Hello World");
+// creating a normal alert in js
+
 
 // let a = 10;
 // let b = 5;
 // let c = a+b;
 // alert(c);
+// adding two numbers
 
 // let num = prompt("enter the number");
 // let square_num = num*num;
 // console.log("the square is",square_num);
+// getting user input and squaring them
 
 // let names = "divyan"
 
@@ -19,17 +23,20 @@
 // else{
 //     console.log("match not found");
 // }
+// checking conditional statements
+
 
 // for (let i = 1; i <=5; i++) {
 //    console.log(i);
 // }
+// printing numbers  between a range using for loop
 
 // let i=1
 // while (i<2) {
 // console.log("hello");
 // break;
 // }
-
+// printing hello using while loop
 
 // let num1 =10;
 // let num2 =20;
@@ -38,11 +45,13 @@
 //     let c = a+b;
 //     console.log(c);
 // }
+// adding two numbers using function
 
 // add(num1,num2);
+// calling the function
 
 // document.querySelector('h1').style.color="red";
-
+// changing the color of h1 using js
 
 // function gettingInput(){
 //      let username = document.getElementById("uName").value;
@@ -50,4 +59,5 @@
 //     document.querySelector("h2").innerText=username;
 //     document.querySelector("h3").innerText=password;
 // }
+// getting input from user using js function
 
