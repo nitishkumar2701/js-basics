92576c15f4980ff7eaa6488a692ec3082b137575
Nitish Kumar S <62009174+nitishkumar2701@users.noreply.github.com>
2025-04-19 13:29:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ let square_num = num*num;
 console.log("the square is",square_num);
 // getting user input and squaring them
 
-let names = "divyan"
+let names = "nitish"
 
 let str = prompt("enter the name:");
 
@@ -60,4 +60,45 @@ function gettingInput(){
     document.querySelector("h3").innerText=password;
 }
 // getting input from user using js function
+
+// Main Functions of JS
+var numbers = [3, 56, 2, 48, 5];
+
+//Map -Create a new array by doing something with each item in an array.
+function tentimes(num) {
+  return num * 10;
+}
+let ten_multiples = numbers.map(tentimes);
+console.log(ten_multiples);
+
+//Filter - Create a new array by keeping the items that return true.
+let even_numbers = numbers.filter(function (num) {
+  if (num % 2 == 0) {
+    return num;
+  }
+});
+console.log(even_numbers);
+
+//Reduce - Accumulate a value by doing something to each item in an array.
+let sum_of_numbers = numbers.reduce(function (accumulator, currentNumber) {
+  return accumulator + currentNumber;
+});
+console.log(sum_of_numbers);
+
+//Find - find the first item that matches from an array.
+var first_odd_number = numbers.find(function (num) {
+  if (num % 2 != 0) {
+    return num;
+  }
+});
+console.log(first_odd_number);
+
+//FindIndex - find the index of the first item that matches.
+var first_odd_number_index = numbers.findIndex(function (num) {
+  if (num % 2 != 0) {
+    return num;
+  }
+});
+console.log(first_odd_number_index);
+
 
